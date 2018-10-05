bb629c56bdc802773f99cd4d9b44d964638b6133
iferres <igferres@gmail.com>
2018-10-05 14:43:46 -0300

    fix .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #
 .onLoad <- function(libname, pkgname) {
-  sys.file <- system.file('inst/extdata', 'codon.subst.mat.rds', package = 'pansimulatoR')
+  sys.file <- system.file('extdata', 'codon.subst.mat.rds', package = 'pansimulatoR')
   .codon.subst.mat <- readRDS(sys.file)
   assign(".codon.subst.mat", .codon.subst.mat, envir = parent.env(environment()))
 }
